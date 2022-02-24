6dc1b106c2b696921e6a4155c37b3bc3ea0398cc
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-24 09:16:21 -0300

    - Add Movie model in the domain targer

---
+++ Domian/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Domain
+//
+//  Created by Luiz Diniz Hammerli on 24/02/22.
+//
+
+import Foundation
+
+public struct Movie: Codable {
+    let id: Int
+    let title: String
+    let poster_path: String?
+}

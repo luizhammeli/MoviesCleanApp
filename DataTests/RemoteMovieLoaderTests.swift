cd4908b958eb6f9d9e6a5fff9a4533d4990ace37
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-24 10:27:17 -0300

    - Create makeSUT method the encapsulate the SUT creation logic - Add helper methods to create some dependencies

--- DataTests/RemoteMovieLoaderTests.swift
+++ DataTests/RemoteMovieLoaderTests.swift
@@ -7,12 +7,41 @@
 
 import Foundation
 import XCTest
+import Domain
 import Data
+
+final class RemoteMovieLoader: MovieLoader {
+    let url: URL
+    let httpClient: HttpGetClient
+    
+    public init(url: URL, httpClient: HttpGetClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    func load(completion: @escaping (MovieLoader.Result) -> Void) {
+        
+    }
+}
 
 final class RemoteMovieLoaderTests: XCTestCase {
     
 }
 
+// MARK: - Helpers
 private extension RemoteMovieLoaderTests {
+    func makeSut() -> (RemoteMovieLoader, HttpGetClientSpy) {
+        let clientSPY = HttpGetClientSpy()
+        let sut = RemoteMovieLoader(url: anyURL(), httpClient: clientSPY)
+        return (sut, clientSPY)
+    }
     
+    func anyURL(stringValue: String = "https://test.com") -> URL {
+        return URL(string: stringValue)!
+    }
+}
+
+final class HttpGetClientSpy: HttpGetClient {
+    func get(to url: URL, completion: @escaping (Result<Data?, HttpError>) -> Void) {
+        
+    }
 }

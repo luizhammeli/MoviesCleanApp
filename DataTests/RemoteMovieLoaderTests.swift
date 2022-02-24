60f8b4f2880d5a0741b28e07fbd22685e8b9b3e8
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-24 09:32:49 -0300

    - Creates RemoteMovieLoaderTests file

---
+++ DataTests/RemoteMovieLoaderTests.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteMovieLoaderTests.swift
+//  DataTests
+//
+//  Created by Luiz Diniz Hammerli on 24/02/22.
+//
+
+import Foundation
+import XCTest
+import Data
+
+final class RemoteMovieLoaderTests: XCTestCase {
+    
+}
+
+private extension RemoteMovieLoaderTests {
+    
+}

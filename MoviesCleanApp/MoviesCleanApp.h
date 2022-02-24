87a5017a89b52c833377e969bf76f719bc230cfb
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-24 09:07:10 -0300

    Initial Commit

---
+++ MoviesCleanApp/MoviesCleanApp.h
@@ -0,0 +1,18 @@
+//
+//  MoviesCleanApp.h
+//  MoviesCleanApp
+//
+//  Created by Luiz Diniz Hammerli on 24/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MoviesCleanApp.
+FOUNDATION_EXPORT double MoviesCleanAppVersionNumber;
+
+//! Project version string for MoviesCleanApp.
+FOUNDATION_EXPORT const unsigned char MoviesCleanAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoviesCleanApp/PublicHeader.h>
+
+

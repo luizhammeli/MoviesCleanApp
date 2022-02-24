1156e218e0bffcd076903d00bef861eaf287b361
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-24 13:42:17 -0300

    Extract HttpGetClientSpy to its own file

---
+++ DataTests/Http/HttpGetClientSpy.swift
@@ -0,0 +1,29 @@
+//
+//  HttpGetClientSpy.swift
+//  DataTests
+//
+//  Created by Luiz Diniz Hammerli on 24/02/22.
+//
+
+import Foundation
+import Data
+
+final class HttpGetClientSpy: HttpGetClient {
+    private var messages: [(url: URL, completion: (HttpGetClient.Result) -> Void)] = []
+    
+    var requestedURLS: [URL] {
+        return messages.map { $0.url }
+    }
+    
+    var messagesCount: Int {
+        messages.count
+    }
+    
+    func get(to url: URL, completion: @escaping (HttpGetClient.Result) -> Void) {
+        messages.append((url, completion))
+    }
+    
+    func complete(with expectedResult: HttpGetClient.Result, at index: Int = 0) {
+        messages[index].completion(expectedResult)
+    }
+}

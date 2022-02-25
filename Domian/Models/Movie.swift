845708286c43d0c860e2d9cb6cd6230ddb9b77fd
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-25 15:14:44 -0300

    - Add unit tests for the presentation layer

--- Domian/Models/Movie.swift
+++ Domian/Models/Movie.swift
@@ -8,17 +8,17 @@
 import Foundation
 
 public struct Movie: Codable, Equatable {
-    let id: Int
-    let title: String
-    let posterPath: String?
+    public let id: Int
+    public let title: String
+    public let posterPath: String
     
-    public init(id: Int, title: String, posterPath: String?) {
+    public init(id: Int, title: String, posterPath: String) {
         self.id = id
         self.title = title
         self.posterPath = posterPath
     }
     
-    enum CodingKeys: String, CodingKey {
+    public enum CodingKeys: String, CodingKey {
         case id, title
         case posterPath = "poster_path"
     }
--- PresentationTests/MovieViewPresenterTests.swift
+++ PresentationTests/MovieViewPresenterTests.swift
@@ -10,30 +10,136 @@ import Data
 import Domain
 //import Presentation
 
+public struct MovieLoadingViewModel {
+    public let isLoading: Bool
+}
+
+public struct MovieViewModel: Equatable, Hashable {
+    public let title: String
+    public let imageURL: URL
+}
+
+public protocol MovieLoadingView {
+    func display(viewModel: MovieLoadingViewModel)
+}
+
+public protocol MovieView {
+    func display(movies: [MovieViewModel])
+}
+
 final class MovieViewPresenter {
-    private let loader: MovieLoader
+    private let imageBaseURL: String
     
-    public init(loader: MovieLoader) {
+    private let loader: MovieLoader
+    private let loadingView: MovieLoadingView
+    private let movieView: MovieView
+    
+    public init(imageBaseURL: String, loader: MovieLoader, loadingView: MovieLoadingView, movieView: MovieView) {
         self.loader = loader
+        self.loadingView = loadingView
+        self.movieView = movieView
+        self.imageBaseURL = imageBaseURL
+    }
+    
+    func loadMovies() {
+        loadingView.display(viewModel: .init(isLoading: true))
+        loader.load { [weak self] result in
+            guard let self = self else { return }
+            self.loadingView.display(viewModel: .init(isLoading: false))
+            if let movies = try? result.get() {
+                self.movieView.display(movies: self.toMoviesViewModel(movies: movies))
+            }
+        }
+    }
+    
+    private func toMoviesViewModel(movies: [Movie]) -> [MovieViewModel] {
+        movies.compactMap { movie in
+            guard let url = URL(string: "\(imageBaseURL + movie.posterPath)") else { return nil }
+            return .init(title: movie.title, imageURL: url)
+        }
     }
 }
 
 final class MovieViewPresenterTests: XCTestCase {
-    func test_() throws {
+    func test_init_shouldNotLoadMovies() {
+        let (_, loader) = makeSUT()
+        XCTAssertTrue(loader.completions.isEmpty)
+    }
+    
+    func test_didStartLoadingMovies_shouldSendIsLoadingViewMessage() {
+        let viewSpy = MovieViewSpy()
+        let (sut, _) = makeSUT(loadingView: viewSpy)
+        
+        sut.loadMovies()
+        XCTAssertEqual(viewSpy.messages, [.display(isLoading: true)])
+    }
+    
+    func test_didFinishLoadingMoviesWithError_shouldSendIsLoadingViewMessage() {
+        let viewSpy = MovieViewSpy()
+        let (sut, loader) = makeSUT(loadingView: viewSpy)
+        
+        sut.loadMovies()
+        loader.complete(with: .failure(.unexpected), at: 0)
+        XCTAssertEqual(viewSpy.messages, [.display(isLoading: true), .display(isLoading: false)])
+    }
+    
+    func test_didFinishLoadingMoviesWithSuccess_shouldSendIsLoadingViewMessage() {
+        let viewSpy = MovieViewSpy()
+        let baseURL = anyURL()
+        let (sut, loader) = makeSUT(baseURL: baseURL.description, loadingView: viewSpy, movieView: viewSpy)
+        let data = toMovieModels(baseURL: baseURL.description, id: 10, title: "Test Title", posterPath: "\(UUID().description)")
+        
+        sut.loadMovies()
+        loader.complete(with: .success([data.model]), at: 0)
+        XCTAssertEqual(viewSpy.messages, [.display(isLoading: true), .display(isLoading: false), .display(movies: [data.movieViewModel])])
     }
 }
 
 // MARK: Helpers
 private extension MovieViewPresenterTests {
-    func makeSUT() -> (MovieViewPresenter, RemoteMovieLoaderSpy) {
+    func makeSUT(baseURL: String = "test",
+                 loadingView: MovieLoadingView = MovieViewSpy(),
+                 movieView: MovieView = MovieViewSpy()) -> (MovieViewPresenter, RemoteMovieLoaderSpy) {
         let loaderSpy = RemoteMovieLoaderSpy()
-        let sut = MovieViewPresenter(loader: loaderSpy)
+        let sut = MovieViewPresenter(imageBaseURL: baseURL, loader: loaderSpy, loadingView: loadingView, movieView: movieView)
+        checkMemoryLeak(for: sut)
         return (sut, loaderSpy)
+    }
+    
+    func toMovieModels(baseURL: String = "test", id: Int, title: String, posterPath: String) -> (model: Movie, movieViewModel: MovieViewModel) {
+        let movie = anyMovie(id: id, title: title, poster_path: posterPath)
+        let url = URL(string: "\(baseURL + movie.posterPath)")!
+        let movieViewModel = MovieViewModel(title: movie.title, imageURL: url)
+        return (movie, movieViewModel)
     }
 }
 
 final class RemoteMovieLoaderSpy: MovieLoader {
+    var completions: [(MovieLoader.Result) -> Void] = []
+    
     func load(completion: @escaping (MovieLoader.Result) -> Void) {
-        
+        completions.append(completion)
+    }
+    
+    func complete(with result: MovieLoader.Result, at index: Int) {
+        completions[index](result)
+    }
+    
+}
+
+final class MovieViewSpy: MovieLoadingView, MovieView {
+    enum Messages: Equatable, Hashable {
+        case display(isLoading: Bool)
+        case display(movies: [MovieViewModel])
+    }
+    
+    var messages: [Messages] = []
+    
+    func display(viewModel: MovieLoadingViewModel) {
+        messages.append(.display(isLoading: viewModel.isLoading))
+    }
+    
+    func display(movies: [MovieViewModel]) {
+        messages.append(.display(movies: movies))
     }
 }

c4f17d36da8fbc352ab33e3ff6f27a437f6a09ee
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-25 11:07:33 -0300

    - Add initial structure of the presenter tests

---
+++ PresentationTests/MovieViewPresenterTests.swift
@@ -0,0 +1,18 @@
+//
+//  MovieViewPresenterTests.swift
+//  PresentationTests
+//
+//  Created by Luiz Diniz Hammerli on 25/02/22.
+//
+
+import XCTest
+//import Presentation
+
+final class MovieViewPresenter {
+    
+}
+
+final class MovieViewPresenterTests: XCTestCase {
+    func test_() throws {
+    }
+}

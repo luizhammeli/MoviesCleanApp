0beb61449ad5bb479a2981b675ab5d23e2fc73c2
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-25 11:32:45 -0300

    - Add makeSUT method in the MovieViewPresenterTests

--- PresentationTests/MovieViewPresenterTests.swift
+++ PresentationTests/MovieViewPresenterTests.swift
@@ -6,13 +6,34 @@
 //
 
 import XCTest
+import Data
+import Domain
 //import Presentation
 
 final class MovieViewPresenter {
+    private let loader: MovieLoader
     
+    public init(loader: MovieLoader) {
+        self.loader = loader
+    }
 }
 
 final class MovieViewPresenterTests: XCTestCase {
     func test_() throws {
+    }
+}
+
+// MARK: Helpers
+private extension MovieViewPresenterTests {
+    func makeSUT() -> (MovieViewPresenter, RemoteMovieLoaderSpy) {
+        let loaderSpy = RemoteMovieLoaderSpy()
+        let sut = MovieViewPresenter(loader: loaderSpy)
+        return (sut, loaderSpy)
+    }
+}
+
+final class RemoteMovieLoaderSpy: MovieLoader {
+    func load(completion: @escaping (MovieLoader.Result) -> Void) {
+        
     }
 }

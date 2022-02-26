44992e8c0cb15824305acbed7bfd6a7c8683cb83
Luiz Hammerli <luizhammerli@gmail.com>
2022-02-26 05:41:26 -0300

    - Add Main Module

---
+++ Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Main
+//
+//  Created by Luiz Hammerli on 26/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

f5c4865fff71ed2b03294b7105cddea80348ef57
Luiz Hammerli <luizhammerli@gmail.com>
2022-03-01 10:08:21 -0300

    - Add MovieImagePresenterTests file

---
+++ PresentationTests/Presenters/MovieImagePresenterTests.swift
@@ -0,0 +1,13 @@
+//
+//  MovieImagePresenterTests.swift
+//  PresentationTests
+//
+//  Created by Luiz Diniz Hammerli on 01/03/22.
+//
+
+import XCTest
+
+final class MovieImagePresenterTests: XCTestCase {
+    func test_() {        
+    }
+}
